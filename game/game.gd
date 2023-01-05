c1b4a6077445871c1ab7f90792bdb7a98fdaeb66
sugarvoid <gamedev@kitargo.com>
2023-01-05 11:29:52 -0600

    Add fade animation to hazard. Add game over function.

--- game/game.gd
+++ game/game.gd
@@ -5,23 +5,22 @@ const MAX_LIVES: int = 3
 
 var player_score: int 
 var player_lives: int
-
 var player: Player
 
 onready var tmr_start_player: Timer = get_node("TmrStart")
-onready var player_killzone: Area2D = get_node("KillZone")
-onready var lbl_score: RichTextLabel = get_node("HUD/LblScore")
 onready var tmr_move_speed_up: Timer = get_node("TmrSpeedUp")
+
+onready var player_killzone: Area2D = get_node("KillZone")
+
+onready var lbl_score: RichTextLabel = get_node("HUD/LblScore")
+onready var lbl_fps: RichTextLabel = get_node("HUD/LblFps")
 
 
 func _ready():
 	self.player = get_node("Player")
 	self._connect_signals()
-	self.tmr_start_player.start(5)
 	self.tmr_move_speed_up.start(10)
 	_update_score(player_score)
-	
-
 
 func _connect_signals() -> void:
 	self.tmr_start_player.connect("timeout", self, "_start_player")
@@ -30,14 +29,28 @@ func _connect_signals() -> void:
 	self.player.connect("has_hit_target", self, "_update_score")
 
 func _start_player() -> void:
+	$HUD/LblInfo.visible = false
 	self.player.in_play = true
+	
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		if self.player.in_play == false:
+			self.player.in_play = true
+		else:
+			return
 
 func _process(delta):
-	$HUD/LblFps.text = str("FPS: ", Engine.get_frames_per_second())
+	self.lbl_fps.text = str("FPS: ", Engine.get_frames_per_second())
 
 func _on_player_off_screen(body: Node) -> void:
 	if body.get_class() == "Player":
 		print("player off screen.")
+		self._gameover()
+
+func _send_info_to_label(s: String) -> void:
+	$HUD/LblInfo.visible = true
+	$HUD/LblInfo.text = s
 
 func _update_score(score: int):
 	self.player_score += score
@@ -46,3 +59,9 @@ func _update_score(score: int):
 func _speed_up_player() -> void:
 	$AnimationPlayer.play("flash_lbl_speed")
 	self.player.increase_speed()
+
+func _gameover() -> void:
+	_send_info_to_label("Game Over")
+	self.tmr_move_speed_up.stop()
+	remove_child($HazardManager)
+	remove_child($TargetManager)
--- game/object/hazard/hazard.gd
+++ game/object/hazard/hazard.gd
@@ -8,16 +8,21 @@ signal on_player_collide
 onready var _hit_box: Area2D = get_node("Hitbox")
 onready var _trm_lifetime: Timer = get_node("TmrLifetime")
 
+
 var time_on_screen: int = 5
 
-
 func _ready() -> void:
+	self.modulate.a = 0
 	self._connect_signals()
-	self._start_timer()
+	$AnimationPlayer.play("fade_in")
+
+func _physics_process(delta: float) -> void:
+	self.rotation_degrees += 25 * delta
 
 func _connect_signals() -> void:
 	self._hit_box.connect("body_entered", self, "_on_player_collide")
 	self._trm_lifetime.connect("timeout", self, "_on_life_timer_timeout")
+	$AnimationPlayer.connect("animation_finished", self, "_on_anim_finished")
 
 func _start_timer() -> void:
 	self._trm_lifetime.start(self.time_on_screen)
@@ -27,4 +32,13 @@ func _on_player_collide(body: Node) -> void:
 		body.take_damage()
 	
 func _on_life_timer_timeout() -> void:
+	# TODO: Add a fade out animation 
+	### $AnimationPlayer.play_backwards("fade_out")
 	self.emit_signal("on_life_timeout", self)
+	
+
+func _on_anim_finished(anmi_name: String) -> void:
+	match anmi_name:
+		"fade_in":
+			_start_timer()
+			### self.emit_signal("on_life_timeout", self)
